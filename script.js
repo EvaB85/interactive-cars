74e194463231eadc7a2d496ec4a975fbb68db56c
Eva Barrett <Evabarrett23@Gmail.com>
2018-11-19 15:29:29 -0800

    lined up cars

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Let\'s get our race on!')
+
+let redCar = document.getElementsByClassName('car--red')[0];
+let blueCar = document.getElementsByClassName('car--blue')[0];
+// let car = document.getElementsByClassName('car');
+
+let carGo = function(e) {
+  if (e.key === 'd') {  // red car
+    e.preventDefault();
+
+  } else if (e.key === 'k') {  // blue car
+    e.preventDefault();
+  }
+}
+
+window.addEventListener('keyup', function(e) {
+  carGo(e);
+});
