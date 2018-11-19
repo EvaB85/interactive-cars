479938387c764672cf0eaf01ac889747b1165c7c
Eva Barrett <Evabarrett23@Gmail.com>
2018-11-19 15:59:29 -0800

    maded cars move on keyup

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ console.log('Let\'s get our race on!')
 let redCar = document.getElementsByClassName('car--red')[0];
 let blueCar = document.getElementsByClassName('car--blue')[0];
 // let car = document.getElementsByClassName('car');
+//getElementsByClassName returns an array
+let redMoveX = 0;
+let blueMoveX = 0;
 
 let carGo = function(e) {
   if (e.key === 'd') {  // red car
     e.preventDefault();
-
+    redMoveX += 35;
+    // redMoveX = redMoveX + 10;
+    redCar.style.transform = `translateX(${redMoveX}px)`;
+    // redCar.style.transform = 'translateX(' + redMoveX + 'px)';
   } else if (e.key === 'k') {  // blue car
     e.preventDefault();
+    blueMoveX += 35;
+    blueCar.style.transform = `translate(${blueMoveX}px)`;
   }
 }
 
